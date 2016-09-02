e31e2094dd013ff1a3e619be4135a07a930815fb
sreekumar <sreekumar456@gmail.com>
2016-09-02 21:28:42 +0530

    sree

---
+++ src/main/java/com/ft/controller/MinutesController.java
@@ -0,0 +1,50 @@
+package com.ft.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ft.model.Activity;
+import com.ft.model.Exercise;
+import com.ft.service.ExerciseService;
+
+
+@Controller
+public class MinutesController {
+
+	@Autowired
+	private ExerciseService exerciseService;
+	
+	@RequestMapping(value = "/addMinutes",  method = RequestMethod.GET)
+	public String getMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+	
+		return "addMinutes";
+	}
+	
+	@RequestMapping(value = "/addMinutes",  method = RequestMethod.POST)
+	public String addMinutes(@Valid @ModelAttribute ("exercise") Exercise exercise, BindingResult result) {
+		
+		System.out.println("exercise: " + exercise.getMinutes());
+		System.out.println("exercise activity: " + exercise.getActivity());
+		
+		if(result.hasErrors()) {
+			return "addMinutes";
+		}
+		
+		return "addMinutes";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+		return exerciseService.findAllActivities();
+	}
+	
+}
---
+++ src/main/java/com/ft/service/ExerciseService.java
@@ -0,0 +1,11 @@
+package com.ft.service;
+
+import java.util.List;
+
+import com.ft.model.Activity;
+
+public interface ExerciseService {
+
+	List<Activity> findAllActivities();
+
+}
